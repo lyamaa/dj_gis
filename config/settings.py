b4134fbcc53374fcf3a7b8fc7a309d61838729a3
surajmt8848 <suraj8848mt@gmail.com>
2021-06-04 08:46:52 +0545

    config static and media path

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
