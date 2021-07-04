385e6a9a7c4cc261ec2279b8e50cc3c2f26a0a7d
surajmt8848 <suraj8848mt@gmail.com>
2021-07-04 11:11:08 +0545

    add model fields

--- location/models.py
+++ location/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Hotel(models.Model):
@@ -7,7 +8,18 @@ class Hotel(models.Model):
     address = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    street_1 = models.CharField(max_length=200)
+    street_2 = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    location = models.PointField(null=True)
     location = models.PointField(null=True)
 
-    def __str__(self) -> str:
-        return self.name
+    class Meta:
+        verbose_name = _("Hotel")
+        verbose_name_plural = _("Hotels")
+
+    def __str__(self):
+        return f"{self.street_1}, {self.city}, {self.state}, {self.country}"
